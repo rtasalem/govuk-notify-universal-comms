f55b5bde68a4b6725cd8ba9afb2bce0fe7692112
Rana Salem <ranatasalem@gmail.com>
2024-11-11 22:18:54 +0000

    004 Send email (#4)

--- app/messages/handle-message.js
+++ app/messages/handle-message.js
@@ -1,7 +1,9 @@
 import util from 'util'
+import { sendEmail } from './send-email.js'
 
 const handleMessage = async (message, receiver) => {
   try {
+    await sendEmail(message)
     await receiver.completeMessage(message)
     console.log('Message received: ', message.body)
   } catch (error) {
